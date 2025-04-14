383ef5b41bde1b9c5dcd7ff800c8da7f9c7168e5
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2025-04-14 08:19:44 +0800

    add recursive method to find fib sequence

--- main.rb
+++ main.rb
@@ -16,4 +16,15 @@ def fibs(n)
   fib
 end
 
-p fibs(8)
+
+def fibs_rec(n)
+  return [0, 1] if n == 2
+  return [0] if n == 1
+
+  fib_sec = fibs_rec(n - 1)
+  fib_sec << fib_sec[-1] + fib_sec[-2]
+  fib_sec
+end
+
+p fibs(5)
+p fibs_rec(5)
